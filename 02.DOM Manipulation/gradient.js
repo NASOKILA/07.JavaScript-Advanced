9d71b18538f4623d8be6dad07785a854e91937c5
Atanas Kambitov <atanasskambitov@gmail.com>
2018-07-25 04:06:28 +0300

    DOM Manipulation

--- 02.DOM Manipulation/gradient.js
+++ 02.DOM Manipulation/gradient.js
@@ -1,5 +1,4 @@
 
-
 function attachGradientEvents() {
 
     let gradient = document.getElementById('gradient-box');
@@ -8,14 +7,13 @@ function attachGradientEvents() {
     gradient.addEventListener('mousemove', gradientMove);
     gradient.addEventListener('mouseout', gradientOut);
 
-    function gradientMove (event) {
-        let power = event.offsetX / (event.target.clientWidth -1);
+    function gradientMove(event) {
+        let power = event.offsetX / (event.target.clientWidth - 1);
         power = Math.trunc(power * 100);
-        result.textContent =  Math.round(power) + '%';
+        result.textContent = Math.round(power) + '%';
     }
 
     function gradientOut(event) {
         document.getElementById('result').textContent = "";
     }
-
 }
